8dc5d8534e0b35ec1f924b8c1695668b6c86eebb
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2021-01-03 14:51:38 +0500

    Update fd.py

--- fd.py
+++ fd.py
@@ -1,24 +1,28 @@
 import json
 
-with open("yedata.json", "r") as file:
+with open("fddata.json", "r") as file:
     text = json.load(file)
-
+    #print(text)
 for i in text["cafe"]:
     print("cafe ", min(i["price"]), sum(i["price"]) // len(i["price"]), max(i["price"]), len(i["price"]), sum(i["price"]))
-
+    for j in i.keys():
+        print("{} - {}".format(j, len(i[j])))
 for i in text["burgerking"]:
     print("burgerking ", min(i["price"]), sum(i["price"]) // len(i["price"]), max(i["price"]), len(i["price"]),sum(i["price"]) )
-
+    for j in i.keys():
+        print("{} - {}".format(j, len(i[j])))
 for i in text["macdonalds"]: 
     print("macdonalds ", min(i["price"]), sum(i["price"]) // len(i["price"]), max(i["price"]), len(i["price"]),sum(i["price"]) )
-
+    for j in i.keys():
+        print("{} - {}".format(j, len(i[j])))
 for i in text["cafe"]:
-    l1 = len(i["price"])
+    p1 = len(i["price"])
     s1 = sum(i["price"])
     for i in text["burgerking"]:
-        l2 = len(i["price"])
+        p2 = len(i["price"])
         s2 = sum(i["price"])
         for i in text["macdonalds"]:
-            l3 = len(i["price"])
+            p3 = len(i["price"])
             s3 = sum(i["price"])
-            print("all ", (s1 + s2 + s3) // (l1 + l2 + l3), l1 + l2 + l3, s1 + s2 + s3)
+            print("all ", (s1 + s2 + s3) // (p1 + p2 + p3), p1 + p2 + p3, s1 + s2 + s3)
+
