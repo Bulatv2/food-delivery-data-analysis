edee3cbc71a8759cd268c4b4a7bd9040bc7ec7c4
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2021-02-06 15:50:49 +0500

    Update fd.py

--- fd.py
+++ fd.py
@@ -5,7 +5,7 @@ with open("fddata.json", "r") as file:
     #print(text)
 for i in text["cafe"]:
     d1 = {}
-    print("cafe ", min(i["price"]), sum(i["price"]) // len(i["price"]), max(i["price"]), len(i["price"]), sum(i["price"]))
+    print("cafe ","min", min(i["price"]), "average", sum(i["price"]) // len(i["price"]), "max", max(i["price"]), "total orders", len(i["price"]), "total amount", sum(i["price"]))
     for j in i.keys():
         if j != "price":
             d1[j] = sum(i[j])
@@ -16,7 +16,7 @@ for i in text["cafe"]:
     for i in listd1:
         print(i[0], "-", i[1])
 for i in text["burgerking"]:
-    print("burgerking ", min(i["price"]), sum(i["price"]) // len(i["price"]), max(i["price"]), len(i["price"]),sum(i["price"]))
+    print("burgerking ","min", min(i["price"]), "average", sum(i["price"]) // len(i["price"]), "max", max(i["price"]), "total orders", len(i["price"]), "total amount", sum(i["price"]))
     d2 = {}
     for j in i.keys():
         if j != "price":
@@ -28,7 +28,7 @@ for i in text["burgerking"]:
     for i in listd2:
         print(i[0], "-", i[1])
 for i in text["macdonalds"]: 
-    print("macdonalds ", min(i["price"]), sum(i["price"]) // len(i["price"]), max(i["price"]), len(i["price"]),sum(i["price"]))
+    print("macdonalds ","min", min(i["price"]), "average", sum(i["price"]) // len(i["price"]), "max", max(i["price"]), "total orders", len(i["price"]), "total amount", sum(i["price"]))
     d3 = {}
     for j in i.keys():
         if j != "price":
@@ -48,4 +48,4 @@ for i in text["cafe"]:
         for i in text["macdonalds"]:
             p3 = len(i["price"])
             s3 = sum(i["price"])
-            print("all ", (s1 + s2 + s3) // (p1 + p2 + p3), p1 + p2 + p3, s1 + s2 + s3)
+            print("all ", "average", (s1 + s2 + s3) // (p1 + p2 + p3), "total oders", p1 + p2 + p3, "total amount", s1 + s2 + s3)
